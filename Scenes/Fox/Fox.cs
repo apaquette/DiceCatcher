abb01a123a4951722727671f7a39ebdc4502e68f
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-06-23 22:57:15 -0300

    refactor movement controls

--- Scenes/Fox/Fox.cs
+++ Scenes/Fox/Fox.cs
@@ -3,38 +3,23 @@ using Godot;
 public partial class Fox : Area2D
 {
 	[Export] public Sprite2D FoxSprite;
-	private const float SPEED = 3f;
+	private const float SPEED = 200f;
+	private float move = 0f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 	}
-
-    // public override void _Input(InputEvent @event)
-    // {
-	// 	// Right arrow key pressed
-    //     if (@event.IsActionPressed("ui_right"))
-	// 	{
-	// 		Position += new Vector2(SPEED, 0);
-	// 	}
-	// 	// Left arrow key pressed
-	// 	if (@event.IsActionPressed("ui_left"))
-	// 	{
-	// 		Position -= new Vector2(SPEED, 0);
-	// 	}
-    // }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(Input.IsActionPressed("ui_right"))
-		{
-			FoxSprite.FlipH = true;
-			Position += new Vector2(SPEED, 0);
-		}
-		else if(Input.IsActionPressed("ui_left"))
-		{
-			FoxSprite.FlipH = false;
-			Position -= new Vector2(SPEED, 0);
-		}
+		
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		move = Input.GetAxis("ui_left", "ui_right");
+		Position += new Vector2(move * SPEED * (float)delta, 0);
+		FoxSprite.FlipH = move > 0;
 	}
 }
