d4cd7f877e29b7c0ffe8e26fc459863e2871fcf3
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-06-23 22:21:23 -0300

    initial commit

---
+++ Scenes/Dice/Dice.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+public partial class Dice : Area2D
+{
+	[Export] public Sprite2D DiceSprite;
+	private const float SPEED = 100f;
+	private const float ROTATIONSPEED = 4f;
+	private int RotationDirection = GD.Randi() % 2 == 0 ? 1 : -1;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        DiceSprite.GlobalPosition += new Vector2(0, 1) * (float)delta * SPEED;
+		DiceSprite.GlobalRotation += ROTATIONSPEED * (float)delta * RotationDirection;
+    }
+}
